3e0801b49730c09e74ad7baf5ba8feb6f6442a03
divyank0203 <divyankh444@gmail.com>
2025-07-05 16:48:01 +0530

    components added

--- jobs/src/App.jsx
+++ jobs/src/App.jsx
@@ -1,8 +1,32 @@
 import React from 'react'
+import Navbar from './components/navbar'
+import Hero from './components/Hero'
+import Maincards from './components/Maincards'
+import Jobcards from './components/Jobcards'
 
 const App = () => {
   return (
-    <div>App</div>
+    <>
+    <Navbar />
+    <Hero
+    title = 'Employ and Get Employed'/>
+    
+    <Maincards/>
+
+    <Jobcards/>
+    
+
+
+
+    <section className="m-auto max-w-lg my-10 px-6">
+      <a
+        href="jobs.html"
+        className="block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700"
+        >View All Jobs</a
+      >
+    </section>
+
+    </>
   )
 }
 
